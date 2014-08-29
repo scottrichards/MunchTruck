fd31e9b657454176189823e9c48c65bd49344078
Scott Richards <scott.srichards@gmail.com>
2014-08-28 23:21:26 -0700

    Got Map Starting to Work

---
+++ MunchTruck/ViewControllers/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  MunchTruck
+//
+//  Created by Scott Richards on 8/28/14.
+//  Copyright (c) 2014 Scott Richards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
